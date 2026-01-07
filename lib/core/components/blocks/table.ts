e15e7284a04dceac503408898f0a3babee716aad
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-01-07 11:27:22 +0100

    chore: made Table consume markdown inside instead of JSON
    The old API was horrible and should not have existed. This is so much cleaner.

--- lib/core/components/blocks/table.ts
+++ lib/core/components/blocks/table.ts
@@ -1,133 +1,92 @@
-import * as zod from 'zod';
+import { Marked, type RendererObject } from 'marked';
 import { Component } from '#lib/core/components/class';
 import { Logger } from '#lib/logger';
-
-type printable = string | number | boolean | null;
-
-const toArrayOfData = function toArrayOfData(
-  data: unknown,
-  context: Component.Interface
-): readonly Readonly<Record<string, printable>>[] {
-  const result = zod
-    .record(zod.string(), zod.string().or(zod.number()).or(zod.boolean()).or(zod.null()))
-    .array()
-    .readonly()
-    .safeParse(data);
-
-  if (result.success) return result.data;
-  throw new Error(
-    `${context.name} at ${context.path} expected data to be an array of records. instead found: ${JSON.stringify(data)}`,
-    {
-      cause: result.error,
-    }
-  );
-};
-
-const valueToString = function valueToString(value: printable): string {
-  // eslint-disable-next-line no-inline-comments
-  return /*HTML*/ `
-    <td class="px-4 py-2 border-b">
-      ${value}
-    </td>
-  `;
-};
-
-const rowToString = function rowToString(row: Readonly<Record<string, printable>>): string {
-  // eslint-disable-next-line no-inline-comments
-  return /*HTML*/ `
-    <tr>
-      ${Object.values(row).map(valueToString).join('\n')}
-    </tr>
-  `;
-};
-
-const headersToString = function headersToString(header: string): string {
-  // eslint-disable-next-line no-inline-comments
-  return /*HTML*/ `
-    <th class="px-4 py-2 text-left border-b">
-      ${header}
-    </th>
-  `;
-};
-
-const parsers = {
-  json: JSON.parse
-} as const;
-
-const getExtension= function getExtension(value: string, context: Component.Interface): string {
-  if (value.includes('.')) {
-    const extension = value.split('.').pop()?.toLowerCase() ?? '';
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
-    if (extension !== '' && extension in parsers) return extension as keyof typeof parsers;
-    Logger.warn(`${context.name} at ${context.path}: Could not `)
-  }
-
-  return 'json';
-};
 
 /**
  * A component that shows the keys in an array of objects.
  */
 @Component.register.using({ plugin: false })
 export class Table extends Component {
-  /** The data for the table. An array of objects. */
-  readonly #data = Component.utils.extract({
-    aliases: ['file', 'url', 'data'],
-    context: this,
-    missing: 'error',
+  /** Renders markdown tables, and nothing else. */
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  public static readonly parser = new Marked({
+    breaks: false,
+    gfm: true,
+    renderer: {
+      blockquote: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      br: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      code: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      codespan: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      def: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      del: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      em: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      heading: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      hr: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      html: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      image: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      link: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      list: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      listitem: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+      paragraph({ tokens }): string {
+        return this.parser.parseInline(tokens);
+      },
+      space: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      strong: ({ raw }: { readonly raw: string }): typeof raw => raw,
+      text: ({ raw }: { readonly raw: string }): typeof raw => raw,
+    } satisfies Omit<Required<RendererObject>, 'table' | 'tablerow' | 'tablecell' | 'checkbox'>,
   });
 
-  /** The type of data that the file can be. */
-  readonly #format = Component.utils.extract({
-    aliases: ['format', 'data-type'],
-    context: this,
-    fallback: 'json',
-    transform: Component.utils.transform.enum(
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
-      Object.keys(parsers) as (keyof typeof parsers)[]
-    ),
-  });
+  // eslint-disable-next-line no-inline-comments, @typescript-eslint/naming-convention
+  public static readonly style = /* CSS */ `
+    table {
+      --line-width: calc(var(--unit) / 8);
+      --padding-x:  calc(var(--unit) * 2); 
+      --padding-y:  calc(var(--unit) / 2);
 
-  @Component.utils.children.reject
-  // eslint-disable-next-line jsdoc/require-jsdoc
-  public async render(): Promise<string> {
-    const url = await Component.utils.toDataURL(this.#data);
+      border-collapse: collapse;
+      border: none;
+      overflow: hidden;
 
-    const { meta, data } = /^data:(?<meta>.*?),(?<data>.*)$/su.exec(url)?.groups ?? {};
-    if (!data) {
-      const maxTokens = 20;
-      throw new Error(
-        `${this.name} at ${this.path} did not get data out of the data URL: "${url.slice(0, maxTokens)}..."`
-      );
+      margin: var(--unit) auto;
     }
 
-    const isBase64 = meta.includes('base64');
-
-    // eslint-disable-next-line no-ternary
-    const decoded = isBase64
-      ? Buffer.from(data, 'base64').toString('utf-8')
-      : decodeURIComponent(data);
-
-    const parsed = toArrayOfData(parsers[this.#format](decoded), this);
-    if (parsed.length === 0) {
-      throw new Error(`${this.name} at ${this.path} receive 0 rows of data`);
+    th,
+    td {
+      padding: var(--padding-y) var(--padding-x);
+      border-right: var(--line-width) solid var(--foreground);
+      border-bottom: var(--line-width) solid var(--foreground);
     }
 
-    const headers = Object.keys(parsed[0]).map(headersToString).join('\n');
-    const rows = parsed.map(rowToString).join('\n');
+    th {
+      text-align: center;
+    }
+
+    th:last-child,
+    td:last-child {
+      border-right: none;
+    }
+
+    tbody tr:last-child td {
+      border-bottom: none;
+    }
+  `;
+
+  @Component.utils.children.require
+  // eslint-disable-next-line jsdoc/require-jsdoc, @typescript-eslint/class-methods-use-this
+  public async render({ children }: Component.RenderArguments): Promise<string> {
+    const input = (children?.() ?? '').split('\n');
+    const trimmed = input.map((line) => line.trim()).join('\n');
+    const table = await Table.parser.parse(trimmed);
 
     // eslint-disable-next-line no-inline-comments
     return /*HTML*/ `
-      <table class="min-w-full border border-foreground rounded-xl overflow-hidden">
-        <thead class="bg-primary">
-          <tr>
-            ${headers}
-          </tr>
-        </thead>
-        <tbody>
-          ${rows}
-        </tbody>
-      </table>
+      <div class="block">
+        ${table}
+        <style scoped>
+          ${Table.style}
+        </style>
+      </div>
     `;
   }
 
