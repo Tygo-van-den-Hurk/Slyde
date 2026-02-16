681ffb104bb653742666d58af0d6f90e02b9a50c
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2026-02-16 14:10:04 +0100

    test: move the cli also into base command
    The previous version searched for it in main cli file which now just executes
    the program.

--- test/src/commands/base.test.ts
+++ test/src/commands/base.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { cli } from '#src/cli';
+import { cli } from '#src/commands/base';
 
 describe('program', () => {
   // Tests relating `program`:
